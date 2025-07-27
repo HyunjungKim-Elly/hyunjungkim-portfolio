61c8a837fd79b0fd4204e8895ebe48fad99f35e1
Hyunjung Kim <ujujo@naver.com>
2025-07-27 17:26:24 +0900

    sitemap 추가

---
+++ next-sitemap.config.ts
@@ -0,0 +1,8 @@
+import type { IConfig } from 'next-sitemap'
+
+const config: IConfig = {
+    siteUrl: 'https://hyunjungkim-portfolio.vercel.app/',
+    generateRobotsTxt: true,
+}
+
+export default config
