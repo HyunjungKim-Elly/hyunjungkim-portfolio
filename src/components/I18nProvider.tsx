9d3c10959513be694ed2201622e035fe4372440b
Hyunjung Kim <ujujo@naver.com>
2025-08-07 18:14:54 +0900

    스크롤 버그 수정

--- src/components/I18nProvider.tsx
+++ src/components/I18nProvider.tsx
@@ -1,20 +1,20 @@
-'use client'
+"use client";
 
-import { ReactNode, useEffect } from 'react'
-import { I18nextProvider } from 'react-i18next'
-import i18n from '@/libs/i18n'
+import { ReactNode, useEffect } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/libs/i18n";
 
 interface Props {
-  children: ReactNode
-  locale: string
+  children: ReactNode;
+  locale: string;
 }
 
 export default function I18nProvider({ children, locale }: Props) {
-  // useEffect(() => {
-  //   if (i18n.language !== locale) {
-  //     i18n.changeLanguage(locale)
-  //   }
-  // }, [locale])
+  useEffect(() => {
+    if (i18n.language !== locale) {
+      i18n.changeLanguage(locale);
+    }
+  }, [locale]);
 
-  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
--- src/hooks/useScrollHashUpdater.ts
+++ src/hooks/useScrollHashUpdater.ts
@@ -1,36 +1,37 @@
-'use client'
-import { useEffect } from 'react'
+"use client";
+import { useEffect } from "react";
 
-const sectionIds = ['about', 'experience', 'projects']
+const sectionIds = ["about", "experience", "projects"];
 
 export const useScrollHashUpdater = () => {
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        const id = entry.target.id
-                        history.replaceState(null, '', `#${id}`)
-                    }
-                })
-            },
-            {
-                threshold: 0.5, // 화면의 절반 이상 보이면 hash 반영
-            }
-        )
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const id = entry.target.id;
+            history.replaceState(null, "", `#${id}`);
+          }
+        });
+      },
+      {
+        rootMargin: "-40% 0px -60% 0px",
+        threshold: 0,
+      }
+    );
 
-        sectionIds.forEach((id) => {
-            const el = document.getElementById(id)
-            if (el) observer.observe(el)
-        })
+    sectionIds.forEach(id => {
+      const el = document.getElementById(id);
+      if (el) observer.observe(el);
+    });
 
-        return () => {
-            sectionIds.forEach((id) => {
-                const el = document.getElementById(id)
-                if (el) observer.unobserve(el)
-            })
-        }
-    }, [])
+    return () => {
+      sectionIds.forEach(id => {
+        const el = document.getElementById(id);
+        if (el) observer.unobserve(el);
+      });
+    };
+  }, []);
 
-    return null
-}
+  return null;
+};
